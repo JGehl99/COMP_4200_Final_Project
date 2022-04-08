bf2f3d243545b4f9200d26a33925a2e02f906a69
Joshua Gehl <jgehl99@gmail.com>
2022-04-07 22:17:24 -0400

    Created Timetable UI and fragment

--- app/src/main/java/com/team3/comp_4200_final_project/TimetableFragment.kt
+++ app/src/main/java/com/team3/comp_4200_final_project/TimetableFragment.kt
@@ -1,16 +1,29 @@
 package com.team3.comp_4200_final_project
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class TimetableFragment : Fragment() {
 
+    private lateinit var classSearchFAB: FloatingActionButton;
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_timetable,container,false)
+        val view =  inflater.inflate(R.layout.fragment_timetable,container,false)
+
+        classSearchFAB = view.findViewById(R.id.class_search_FAB)
+
+        classSearchFAB.setOnClickListener {
+            val i = Intent(view.context, SearchActivity::class.java)
+            startActivity(i)
+        }
+
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
