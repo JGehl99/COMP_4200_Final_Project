39d70e442c6d7997e2729c135afd6b85942b39bf
Joshua Gehl <jgehl99@gmail.com>
2022-04-09 16:03:26 -0400

    Updated TimetableFragment.kt and fragment_timetable.xml to incorporate a TabLayout and ViewPager2 to create tabs that the user can swipe between.

--- app/src/main/java/com/team3/comp_4200_final_project/TimetableFragment.kt
+++ app/src/main/java/com/team3/comp_4200_final_project/TimetableFragment.kt
@@ -5,28 +5,59 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class TimetableFragment : Fragment() {
 
-    private lateinit var classSearchFAB: FloatingActionButton;
+    // List of tab names
+    private val tabs = arrayListOf(
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+    )
 
+    // lateinit View objects
+    private lateinit var classSearchFAB: FloatingActionButton
+    private lateinit var fragmentPagerAdapter: TimetableFragmentPagerAdapter
+    private lateinit var viewPager: ViewPager2
+    private lateinit var tabLayout: TabLayout
+
+    // Overriding onCreateView
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        // Get view
         val view =  inflater.inflate(R.layout.fragment_timetable,container,false)
 
-        classSearchFAB = view.findViewById(R.id.class_search_FAB)
+        // TimetableFragmentPagerAdapter extends FragmentStateAdapter()
+        fragmentPagerAdapter = TimetableFragmentPagerAdapter(childFragmentManager, tabs,  lifecycle)
 
+        // Get viewPager View and set the adapter
+        viewPager = view.findViewById(R.id.timetable_view_pager)
+        viewPager.adapter = fragmentPagerAdapter
+
+        // Get tabLayout View and bind it to viewPager with TabLayoutMediator.
+        // The lambda will set the tab text based on the value at that position in the array
+        tabLayout = view.findViewById(R.id.timetable_tab_layout)
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = tabs[position]
+        }.attach()
+
+        // Get classSearchFAB View and set OnClickListener to launch SearchActivity
+        classSearchFAB = view.findViewById(R.id.class_search_FAB)
         classSearchFAB.setOnClickListener {
             val i = Intent(view.context, SearchActivity::class.java)
             startActivity(i)
         }
 
+        // Return view
         return view
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
     }
 }
