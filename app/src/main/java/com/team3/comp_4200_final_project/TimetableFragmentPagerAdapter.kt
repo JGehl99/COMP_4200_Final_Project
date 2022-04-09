e03d224758dc3ce0a6200d64892039991477da6c
Joshua Gehl <jgehl99@gmail.com>
2022-04-09 16:01:52 -0400

    Created TimetableFragmentPagerAdapter that extends FragmentStateAdapter for ViewPager2 and TabLayoutMediator found in TimetableFragment.kt

---
+++ app/src/main/java/com/team3/comp_4200_final_project/TimetableFragmentPagerAdapter.kt
@@ -0,0 +1,18 @@
+package com.team3.comp_4200_final_project
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class TimetableFragmentPagerAdapter(
+    fm: FragmentManager,
+    private val dayArrayList: ArrayList<String>,
+    lifecycle: Lifecycle
+) : FragmentStateAdapter(fm, lifecycle) {
+
+    override fun getItemCount(): Int = dayArrayList.size
+
+    override fun createFragment(position: Int): Fragment =
+        DayFragment.newInstance(dayArrayList[position])
+}
