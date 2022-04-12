9428c15f1d308758bc3c1513feb5f0f292756f2a
Bailey Chittle <bachittle@gmail.com>
2022-04-11 20:33:44 -0400

    added rough course searching

--- app/src/main/java/com/team3/comp_4200_final_project/ClassSearchFragment.kt
+++ app/src/main/java/com/team3/comp_4200_final_project/ClassSearchFragment.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -19,6 +20,8 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 class ClassSearchFragment : Fragment() {
 
@@ -49,7 +52,7 @@ class ClassSearchFragment : Fragment() {
             .baseUrl("https://utable.net/")
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
-            .create(UserService::class.java)
+            .create(UTableService::class.java)
 
         var autofillStrings = mutableListOf<String>()
         service.getCourses().enqueue(object : Callback<List<AutofillData>> {
@@ -78,6 +81,42 @@ class ClassSearchFragment : Fragment() {
         var aa = ArrayAdapter<String>(view.context, android.R.layout.simple_dropdown_item_1line, autofillStrings)
         searchBar = view.findViewById(R.id.search_field)
         searchBar.setAdapter(aa)
+
+        var searchButton : Button = view.findViewById(R.id.search_button)
+        searchButton.setOnClickListener {
+            service.getSearchResult(searchBar.text.toString()).enqueue(object : Callback<List<CourseSearchResult>> {
+
+                /* The HTTP call failed. This method is run on the main thread */
+                override fun onFailure(call: Call<List<CourseSearchResult>>, t: Throwable) {
+                    Log.d("TAG_", "An error happened!")
+                    t.printStackTrace()
+                }
+
+                /* The HTTP call was successful, we should still check status code and response body
+                 * on a production app. This method is run on the main thread */
+                override fun onResponse(call: Call<List<CourseSearchResult>>, response: Response<List<CourseSearchResult>>) {
+                    /* This will print the response of the network call to the Logcat */
+                    var results = response.body()
+                    if (results != null) {
+                        arr.clear()
+                        for(result in results) {
+                            Log.d("RESPONSE_", result.toString())
+                            for (section in result.sections) {
+                                if (section.semester_id == 3) // set this to latest semester each time, IK its bad
+                                // todo: make subsections better
+                                if (section.subsections != null && section.subsections.isNotEmpty()) {
+                                    var subsec = section.subsections[0]
+                                    arr.add(
+                                        ClassData(result.courseCode, result.courseName, subsec.startTime, subsec.endTime, subsec.day, subsec.location, subsec.professors.toString())
+                                    )
+                                }
+                            }
+                        }
+                        reAdapter.notifyDataSetChanged()
+                    }
+                }
+            })
+        }
     }
 }
 
@@ -86,10 +125,20 @@ class ClassSearchFragment : Fragment() {
 data class AutofillData(val courseCode: String, val courseName: String, val courseProf: List<Prof>)
 data class Prof(val firstName: String, val lastName: String)
 
+data class CourseSearchResult(val courseCode: String, val courseName: String, val sections: List<Section>)
+
+data class Section(val sectionNum: Int, val sectionType: String, val subsections: List<Subsec>, val semester_id: Int)
+data class Subsec(val day: String, val startTime: String, val endTime: String, val location: String, val professors: List<Prof>)
+
 /* Retrofit service that maps the different endpoints on the API, you'd create one
  * method per endpoint, and use the @Path, @Query and other annotations to customize
  * these at runtime */
-interface UserService {
+interface UTableService {
     @GET("/api/autofill/")
     fun getCourses(): Call<List<AutofillData>>
+
+    @GET("/api/search/?search")
+    fun getSearchResult(
+        @Query("search") search: String
+    ) : Call<List<CourseSearchResult>>
 }
